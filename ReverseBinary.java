ba08f14553f8b1c8ad4aafb01879153a7001472b
kioksy <ante89@gmail.com>
2013-11-26 15:54:43 +0100

    Update ReverseBinary.java

--- ReverseBinary.java
+++ ReverseBinary.java
@@ -3,22 +3,22 @@ public class ReverseBinary{
     private static int biggestNumber = 1000000000;
 	
 	public static void main(String[] args) throws java.io.IOException{
-			try{
-				java.io.BufferedReader stdin = new java.io.BufferedReader(new java.io.InputStreamReader(System.in));
-			    String readLine=stdin.readLine();
-				int numb = Integer.parseInt(readLine);
-					if(numb>0 && numb<=biggestNumber){
-						String outputString = new StringBuffer(Integer.toBinaryString(numb)).reverse().toString();
-						numb = Integer.parseInt(outputString, 2);
-						System.out.println(numb);
-					}else{
-						System.out.println("Your number is out of range");
-					}
-			}catch(NumberFormatException e){
-				System.out.println("you must choose a number(integer) as input");
+		try{
+		    java.io.BufferedReader stdin = new java.io.BufferedReader(new java.io.InputStreamReader(System.in));
+		    String readLine=stdin.readLine();
+	  	    int numb = Integer.parseInt(readLine);
+			if(numb>0 && numb<=biggestNumber){
+				String outputString = new StringBuffer(Integer.toBinaryString(numb)).reverse().toString();
+				numb = Integer.parseInt(outputString, 2);
+				System.out.println(numb);
+			}else{
+				System.out.println("Your number is out of range");
 			}
-			catch(java.io.IOException e){
-                System.out.println("Java IO Exception thrown");
-			}
+		}catch(NumberFormatException e){
+			System.out.println("you must choose a number(integer) as input");
+		}
+		catch(java.io.IOException e){
+       			 System.out.println("Java IO Exception thrown");
 		}
 	}
+}
